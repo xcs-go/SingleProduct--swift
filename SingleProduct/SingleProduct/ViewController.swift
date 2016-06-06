e511d35eff0623e6e75b26f77a3ed12e62808c1d
xcs <13162927913@163.com>
2016-06-06 22:19:29 +0800

    Init

---
+++ SingleProduct/SingleProduct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleProduct
+//
+//  Created by 谢朝森 on 16/6/6.
+//  Copyright (c) 2016年 xcs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
